5c3c890194444a7021ff662c704d91285f200a27
rohangokhale <gokhaler@oregonstate.edu>
2017-01-31 19:00:09 +0000

    Clean Commit

--- dump.py
+++
@@ -1,9 +0,0 @@
-import settings
-import tweepy
-import dataset
-from textblob import TextBlob
-
-db = dataset.connect(settings.CONNECTION_STRING)
-
-result = db[settings.TABLE_NAME].all()
-dataset.freeze(result, format='csv', filename=settings.CSV_NAME)
